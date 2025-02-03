883bd33b5b62616b0f5a392c5256142df6ba9c4f
hitsumo <hitsumo@sicakcikolata.com>
2025-02-03 09:26:12 +0300

    Update rooms.py

--- world/rooms.py
+++ world/rooms.py
@@ -1,26 +1,43 @@
-from evennia import create_object
-from typeclasses.custom_rooms import WelcomeRoom, TrainingRoom
-from typeclasses.custom_objects import Note, CustomChest
-from typeclasses.custom_characters import Mahmud
+from evennia import DefaultRoom
+from evennia.utils import delay
 
-def build_game():
-    # Karşılama Odası
-    welcome_room = create_object(WelcomeRoom, key="Karşılama Odası")
-    welcome_room.db.desc = "Bir geçitten yere düşerek dışarı çıktın. Geçit gümbürtüyle kapandı."
+class OpeningRoom(DefaultRoom):
+    """
+    Oyunun açılış odası.
+    """
+    def at_object_receive(self, obj, source_location):
+        """
+        Oyuncu odaya girdiğinde tetiklenir.
+        """
+        if obj.has_account:  # Oyuncuysa
+            self.msg_contents("|cMerhaba!|n Yeniden Uzak Diyarlarda oyununa hoş geldiniz.")
+            delay(2, self.msg_contents, "Bu oyun yazı tabanlı olup Evennia sistemi kullanılarak oluşturulmuştur.")
+            delay(4, self.msg_contents, "|gHazırım|n yazarak oyuna başlayabilirsin.")
 
-    # Not Kâğıdını ekle
-    note = create_object(Note, key="Not Kâğıdı", location=welcome_room)
 
-    # Eğitim Odası
-    training_room = create_object(TrainingRoom, key="Eğitim Odası")
-    training_room.db.desc = "Oda neredeyse diğer odayla aynı. Bir kişi ve bir sandık dışında farklı bir şey yok."
+class WelcomeRoom(DefaultRoom):
+    """
+    Oyuncunun oyuna başladığı karşılama odası.
+    """
+    def at_object_receive(self, obj, source_location):
+        """
+        Oyuncu odaya girdiğinde tetiklenir.
+        """
+        if obj.has_account:  # Oyuncuysa
+            self.msg_contents("Bir geçitten yere düşerek dışarı çıktın. Geçit gümbürtüyle kapandı.")
+            delay(2, self.msg_contents, "Etrafına bakınıyorsun. Sonra bir ses duydun:")
+            delay(3, self.msg_contents, '"Merhaba Oyuncu! Artık buradasın."')
+            delay(8, self.msg_contents, "Bu odada yapabileceğin temel eylemler ile ilgili bilgi alacağın bir eşya var. "
+                                         "Bu eşya tahta masanın üzerindeki not kâğıdı.")
+            delay(10, self.msg_contents, "Şu iki ipucunu veriyorum sana: 'Bak' ve 'Oku.'")
+            delay(12, self.msg_contents, "Ardından gürültülü bir kahkaha duyuyorsun.")
 
-    # Mahmud'u ekle
-    mahmud = create_object(Mahmud, key="Mahmud", location=training_room)
 
-    # Sandığı ekle
-    chest = create_object(CustomChest, key="Sandık", location=training_room)
-
-    # Odaları bağla
-    welcome_room.db.exits = {"doğu": training_room}
-    training_room.db.exits = {"batı": welcome_room}
+class TrainingRoom(DefaultRoom):
+    """
+    Eğitim odası.
+    """
+    def at_object_receive(self, obj, source_location):
+        if obj.has_account:  # Oyuncuysa
+            self.msg_contents("Mahmud: Merhaba! Görüyorum ki Karşılama Salonundan geldin. "
+                              "Ben Mahmud. Sana temel eğitim vereceğim.")
